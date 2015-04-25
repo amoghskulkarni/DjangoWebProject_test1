524861892c695e91940b221185daedd4230fb86f
ayan.mukhopadhyay@vanderbilt.edu <ayan.mukhopadhyay@vanderbilt.edu>
2015-04-25 01:50:55 -0500

    Test Commit

---
+++ DjangoWebProject_test1/app/static/app/scripts/suggestion.js
@@ -0,0 +1,48 @@
+﻿$(document).ready(function () {
+
+    $('#suggButton').click(function (e) {
+        e.preventDefault();
+        //get radio button values
+        var size = $('input:radio[name=size]:checked').val();
+        var categorical = $('input:radio[name=categorical]:checked').val();
+        var labelled = $('input:radio[name=labelled]:checked').val();
+        var numClusters = $('input:radio[name=numClusters]:checked').val();
+        if (!$("input[name=size]:checked").val()) {
+            alert('Please answer all the questions!');
+        }
+        else if (!$("input[name=categorical]:checked").val()) {
+            alert('Please answer all the questions!');
+        }
+        else if (!$("input[name=labelled]:checked").val()) {
+            alert('Please answer all the questions!');
+        }
+        else if (!$("input[name=size]:numClusters").val()) {
+            alert('Please answer all the questions!');
+        }
+        else{
+            alert("inside this block");
+            alert(String(size));
+            alert(String(categorical));
+            alert(String(labelled));
+            alert(String(numClusters));
+            var DATA = size + "," + categorical+ "," + labelled + "," + numClusters;
+            alert(DATA)
+            $.ajax({
+                url: "/testImageResponse",
+                type: "POST",
+                //data: data,
+                data: { 'data': DATA, csrfmiddlewaretoken: document.getElementsByName('csrfmiddlewaretoken')[0].value },
+                success: function (response) {
+                    alert("In success. Got - " + response);
+                },
+                complete: function (response) {
+                    document.getElementById('imageDiv').style.backgroundImage = response //incorrect line
+                },
+                error: function (xhr, textStatus, thrownError) {
+                    alert("error doing something");
+                }
+            });
+    }
+    });
+
+});
